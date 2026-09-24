02c700021a04ab2b71ac0e2c543d651d030e51cc
mdmanjar <mdmanjar7929@gmail.com>
2026-09-24 10:06:21 +0530

    Time: 827 ms (71.98%), Space: 73.9 MB (45.86%) - LeetHub

--- 3600-maximize-spanning-tree-stability-with-upgrades/3600-maximize-spanning-tree-stability-with-upgrades.py
+++ 3600-maximize-spanning-tree-stability-with-upgrades/3600-maximize-spanning-tree-stability-with-upgrades.py
@@ -1,67 +1,71 @@
+class dsu:
+    def __init__(self,p):
+        self.p=p[:]
+
+    def find(self,a):
+        if self.p[a]<0:return a
+        self.p[a]=self.find(self.p[a])
+        return self.p[a]
+
+    def union(self,a,b):
+        a,b=self.find(a),self.find(b)
+        if a==b:return False
+        if self.p[a]>self.p[b]:
+            a,b=b,a
+        self.p[a]+=self.p[b]
+        self.p[b]=a
+        return True
+
 
 class Solution:
-    def maxStability(self, n: int, edges: List[List[int]], k: int) -> int:
+    def maxStability(self,n:int,edges:List[List[int]],k:int)->int:
+        optional=[]
+        mx=math.inf
+        must=0
+        base=dsu([-1]*n)
+        rem=n-1
 
-        p = [-1] * n
-
-        def find(x):
-            if p[x] < 0:
-                return x
-            p[x] = find(p[x])
-            return p[x]
-
-        def union(a, b):
-            a = find(a)
-            b = find(b)
-
-            if a == b:
-                return False
-
-            if p[a] > p[b]:
-                a, b = b, a
-
-            p[a] += p[b]
-            p[b] = a
-            return True
-
-        optional = []
-        remaining = n - 1
-        min_must = float('inf')
-
-        for u, v, s, m in edges:
+        for u,v,s,m in edges:
             if m:
-                if not union(u,v): return -1
-                remaining-=1
-                min_must = min(min_must,s)
+                if not base.union(u,v):return -1
+                mx=min(mx,s)
+                must+=1
+                rem-=1
             else:
-                optional.append((u, v, s))
+                optional.append((u,v,s))
 
-        if remaining == 0:
-            return min_must
+        if rem==0:return mx
 
-        optional.sort(key=lambda x: x[2], reverse=True)
+        def check(x):
+            uf=dsu(base.p)
+            cnt=0
+            upgrade=0
 
-        chosen = []
+            for u,v,s in optional:
+                if s>=x:
+                    if uf.union(u,v):
+                        cnt+=1
 
-        for u, v, s in optional:
-            if union(u, v):
-                chosen.append(s)
-                remaining -= 1
+            for u,v,s in optional:
+                if s<x and s*2>=x:
+                    if uf.union(u,v):
+                        cnt+=1
+                        upgrade+=1
+                        if upgrade>k:
+                            return False
 
-                if remaining == 0:
-                    break
+            return cnt==rem
 
-        if remaining:
-            return -1
+        left=0
+        right=2*max(s for _,_,s,_ in edges)
+        ans=-1
 
+        while left<=right:
+            mid=(left+right)//2
+            if mid<=mx and check(mid):
+                ans=mid
+                left=mid+1
+            else:
+                right=mid-1
 
-
-        if k >= len(chosen):
-            return min(min_must, 2 * chosen[-1])
-
-
-        return min(
-            min_must,
-            2 * chosen[-1],
-            chosen[-k - 1]
-        )
+        return ans
