536a61ccc875c7eac5acdeff877373131427bebb
Manjar Shaikh <mdmanjar7929@gmail.com>
2026-09-25 19:47:06 +0530

    Time: 83 ms (47.95%), Space: 27.9 MB (64.44%) - LeetHub

--- 0895-maximum-frequency-stack/0895-maximum-frequency-stack.py
+++ 0895-maximum-frequency-stack/0895-maximum-frequency-stack.py
@@ -3,9 +3,6 @@ class FreqStack:
     def __init__(self):
         self.mp=defaultdict(lambda:-1)
         self.stack=[]
-
-        
-
     def push(self, val: int) -> None:
         self.mp[val]+=1
         idx=self.mp[val]
@@ -17,6 +14,8 @@ class FreqStack:
         v=self.stack[-1].pop()
         if not self.stack[-1]:self.stack.pop()
         self.mp[v]-=1
+        if self.mp[v]==-1:
+            del self.mp[v]
         return v
         
 
