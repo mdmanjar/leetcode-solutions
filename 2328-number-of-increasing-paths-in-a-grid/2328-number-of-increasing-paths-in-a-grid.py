d1fe735ef84cdc30d3791aefb990f5c152122453
Manjar Shaikh <mdmanjar7929@gmail.com>
2026-09-25 11:36:23 +0530

    Time: 1099 ms (14.41%), Space: 32.6 MB (98.35%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.py
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.py
@@ -1,16 +1,33 @@
 class Solution:
-    def countPaths(self, grid: List[List[int]]) -> int:
+    def countPaths(self, grid: list[list[int]]) -> int:
         m,n=len(grid),len(grid[0])
-        dp=[[0]*n for _ in range(m)]
+        path=[[1]*n for _ in range(m)]
+        deg=[[0]*n for _ in range(m)]
+        dir=(-1,0,1,0)
+        mod=10**9+7
 
-        dirs=(-1,0,1,0)
+        for i in range(m):
+            for j in range(n):
+                for k in range(4):
+                    u,v=i+dir[k],j+dir[3-k]
+                    if -1<u<m and -1<v<n and grid[i][j]<grid[u][v]:
+                        deg[u][v]+=1
+        
+        q=deque((i,j) for i in range(m) for j in range(n) if deg[i][j]==0)
+        ans=0
+        while q:
+            i,j=q.popleft()
+            ans+=path[i][j]
 
-        def dfs(i,j,prev):
-            if not (-1<i<m and -1<j<n and prev<grid[i][j]):return 0
-            if dp[i][j]:return dp[i][j]
-            dp[i][j]=sum(dfs(i+dirs[k],j+dirs[3-k],grid[i][j]) for k in range(4))+1
-            return dp[i][j]%(10**9+7)
+            for k in range(4):
+                u,v=i+dir[k],j+dir[3-k]
+                if -1<u<m and -1<v<n and grid[i][j]<grid[u][v]:
+                    path[u][v]=(path[i][j]+path[u][v])%mod
+                    deg[u][v]-=1
+                    if deg[u][v]==0:
+                        q.append((u,v))
+        return ans%mod
 
-        return sum(dfs(i,j,0) for i in range(m) for j in range(n))%(10**9+7)
+    
 
         
