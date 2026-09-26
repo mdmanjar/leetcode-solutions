142ce12d4156f8768e4365938ebdff43b31cd03b
Manjar Shaikh <mdmanjar7929@gmail.com>
2026-09-26 11:17:05 +0530

    Time: 7 ms (72.38%), Space: 20.1 MB (44.22%) - LeetHub

--- 1670-design-front-middle-back-queue/1670-design-front-middle-back-queue.py
+++ 1670-design-front-middle-back-queue/1670-design-front-middle-back-queue.py
@@ -1,58 +1,64 @@
-from collections import deque
-
 class FrontMiddleBackQueue:
 
     def __init__(self):
-        self.left = deque()
-        self.right = deque()
-
-    def balance(self):
-        if len(self.left) > len(self.right) + 1:
-            self.right.appendleft(self.left.pop())
-        elif len(self.left) < len(self.right):
+        self.left=deque()
+        self.right=deque()
+    
+    def __balanced(self):
+        if len(self.left)<len(self.right):
             self.left.append(self.right.popleft())
+        if len(self.left)>len(self.right)+1:
+            self.right.appendleft(self.left.pop())
 
-    def pushFront(self, val):
+    def pushFront(self, val: int) -> None:
         self.left.appendleft(val)
-        self.balance()
+        self.__balanced()
+        
 
-    def pushMiddle(self, val):
-        if len(self.left) > len(self.right):
+    def pushMiddle(self, val: int) -> None:
+        if len(self.left)>len(self.right):
             self.right.appendleft(self.left.pop())
         self.left.append(val)
-
-    def pushBack(self, val):
+        self.__balanced()
+        
+    def pushBack(self, val: int) -> None:
         self.right.append(val)
-        self.balance()
+        self.__balanced()
+        
 
-    def popFront(self):
-        if not self.left and not self.right:
-            return -1
+    def popFront(self) -> int:
+        if not self.left:return -1
+        v=self.left.popleft()
+        self.__balanced()
+        return v
+        
 
+    def popMiddle(self) -> int:
+        self.__balanced()
+        if not self.left:return -1
+        v=self.left.pop()
+        self.__balanced()
+        return v
+        
+
+    def popBack(self) -> int:
+        if self.right :
+            v=self.right.pop()
+            self.__balanced()
+            return v
         if self.left:
-            ans = self.left.popleft()
-        else:
-            ans = self.right.popleft()
+            v=self.left.pop()
+            self.__balanced()
+            return v
+        return -1
+        
 
-        self.balance()
-        return ans
 
-    def popMiddle(self):
-        if not self.left and not self.right:
-            return -1
-
-        ans = self.left.pop()
-        self.balance()
-        return ans
-
-    def popBack(self):
-        if not self.left and not self.right:
-            return -1
-
-        if self.right:
-            ans = self.right.pop()
-        else:
-            ans = self.left.pop()
-
-        self.balance()
-        return ans
+# Your FrontMiddleBackQueue object will be instantiated and called as such:
+# obj = FrontMiddleBackQueue()
+# obj.pushFront(val)
+# obj.pushMiddle(val)
+# obj.pushBack(val)
+# param_4 = obj.popFront()
+# param_5 = obj.popMiddle()
+# param_6 = obj.popBack()
